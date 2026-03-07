4e56226f8fb5b01ac3bbc8da5ba13eba2fbbcdc8
spartacus-has-fallen <spartacushasfallen@gmail.com>
2026-03-07 10:15:49 +0530

    Fix Atlas TLS: bypass Nix OpenSSL cipher issue, upgrade motor/pymongo

--- backend/server.py
+++ backend/server.py
@@ -27,7 +27,7 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
+client = AsyncIOMotorClient(mongo_url, tls=True, tlsAllowInvalidCertificates=True)
 db = client[os.environ['DB_NAME']]
 
 # JWT Configuration
